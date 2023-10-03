9f43a45f312f69ad599eb88de773b8b297611f81
krishsuthar1504 <131298127+krishsuthar1504@users.noreply.github.com>
2023-10-03 12:56:56 +0530

    After_Oprators

--- D2_Basics/1.Variables.js
+++ D2_Basics/1.Variables.js
@@ -17,3 +17,51 @@ a = 'krish'
 // we can use both "" and '' their is no difference
 b = 'Krish'+" "+"Suthar" // + for string concatination too!!
 console.log(b)
+
+// You Can Assign Many Variables :
+let p = 10 ,q = 20 , r ="krish";
+
+let k;
+// k is undefined
+// you cant re declare the let variable!!!
+// let k = 10;
+
+
+var redec = "10";
+var redec = "20";
+// you can redeclare the var variable
+
+
+{
+    let let_ex = "10";
+    console.log(let_ex); // it gen o/p
+}
+// console.log(let_ex); error
+
+
+{
+    var var_ex = "var";
+    console.log(var_ex); // it gen o/p
+}
+console.log(var_ex); // no error
+
+
+
+//Hoisting :
+
+car = "xuv";
+var car;
+// you can!!!!
+
+engine = "1800cc"
+// let engine;
+//You Cant
+
+
+
+
+
+const cars = ['1','2']
+cars[0] = ['0'] // you can
+cars.push('3') // you can
+// cars = ['1'] // you cant

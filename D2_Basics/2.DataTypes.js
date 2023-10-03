d9742426f076c4fa1ac54c2cb22598ae30ba34ea
krishsuthar1504 <131298127+krishsuthar1504@users.noreply.github.com>
2023-10-03 13:07:51 +0530

    D2_Finished

---
+++ D2_Basics/2.DataTypes.js
@@ -0,0 +1,41 @@
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+
+// With decimals:
+let x1 = 34.00;
+
+// Without decimals:
+let x2 = 34;
+
+//Exponantial
+let y1 = 123e5;    // 12300000
+let z = 123e-5;   // 0.00123
+
+//BigInt
+let _x = BigInt("123456789012345678901234567890");
+
+
+//Bool
+(x == y)       // Returns true
+(x == z)       // Returns false
+
+

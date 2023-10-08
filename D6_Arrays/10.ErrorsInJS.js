014b8ced8e2b2a36d030d2fde321afc0f260d9b1
krishsuthar1504 <131298127+krishsuthar1504@users.noreply.github.com>
2023-10-08 19:58:49 +0530

    D6_Done

---
+++ D6_Arrays/10.ErrorsInJS.js
@@ -0,0 +1,13 @@
+const { json } = require("express");
+
+try{
+    console.log("hiii")
+}
+catch(err){
+    throw new err;
+}
+
+
+
+JSON.parse();
+JSON.stringify();
---
+++ D6_Arrays/8.Arrays.js
@@ -0,0 +1,2 @@
+const car = [1,2,3,4,5,6,7]
+car.length()

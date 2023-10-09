659b18a0acd9a9dc9505d97c311cc52ce82f476e
krishsuthar1504 <131298127+krishsuthar1504@users.noreply.github.com>
2023-10-09 23:21:30 +0530

    Update 8.Arrays.js

--- D6_Arrays/8.Arrays.js
+++ D6_Arrays/8.Arrays.js
@@ -1,5 +1,5 @@
 const car = [1,2,3,4,5,6,7]
-car.length()
+// car.length()
 
 
 car.sort()
@@ -11,3 +11,14 @@ let text = "";
 for (let x of cars) {
   text += x;
 }
+
+
+
+cars.push("HIiiii");//push to the last element
+console.log(cars);
+cars.pop(); // removes the last element
+console.log(cars)
+cars.shift()//remove the first element
+console.log(cars)
+cars.unshift("BMW")//add the first element
+console.log(cars)

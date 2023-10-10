54b39ca2651a05ffc95a43a04ff11d54b2c30412
krishsuthar1504 <131298127+krishsuthar1504@users.noreply.github.com>
2023-10-10 22:43:13 +0530

    Create CrashPart2.js

---
+++ D7_8_MAIN!!/CrashPart2.js
@@ -0,0 +1,28 @@
+// topics to learn
+// higher order function
+// first class func
+// new keyword
+//iife
+//prototype
+//prototypal inheritance
+//this call apply bind
+//pure & impu fncs
+//closures
+
+// 1.Higher Order Function -  Accept Or Return A Function this is called higher order function
+
+
+//type 1
+function abcd(){
+    return function(){}
+};
+
+
+//type 2
+
+
+function abcd(a){
+    console.log("hiii");
+};
+
+var a = function(){};
